bf25e5cc8200babd4e8bf43a402110ad5b495b01
Felgect <98808479+Felgect@users.noreply.github.com>
2022-12-01 15:57:25 +0500

    Add files via upload

---
+++ Практика 9/PracticWork9/Program.cs
@@ -0,0 +1,18 @@
+﻿using PracticWork9;
+
+List<Phone> phones = new List<Phone>();
+
+
+PersonCreate person = new PersonCreate();
+phones.Add(person.GenerateNote("Дмитрий", 1234));
+
+IndividualCreate individual = new IndividualCreate();
+phones.Add(individual.GenerateNote("ООО Форвард", 1010));
+
+InstitutionCreate institution = new InstitutionCreate();
+phones.Add(institution.GenerateNote("ОКЭИ", 9999));
+
+foreach (var item in phones)
+{
+    Console.WriteLine(item.GetInfo());
+}
